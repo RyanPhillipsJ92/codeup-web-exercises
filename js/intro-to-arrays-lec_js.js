39245ebbb7b19cc6b16d2c3cfe8b2e8d29a7fe9c
Ryan Phillips <ryan.phillips.j92@gmail.com>
2020-10-05 15:52:01 -0500

    completed arrays lesson

--- js/intro-to-arrays-lec_js.js
+++ js/intro-to-arrays-lec_js.js
@@ -25,27 +25,32 @@ console.log("Welcome to intro to arrays");
 // Notice that the array above does *not* have 5 elements, rather the last element is itself an array with 3 elements in it
 
 //TODO TOGETHER: Declare an empty array set equal to the variable `pies` - then console.log `pies`
-
+var pies = [];
+// console.log(pies);
 
 //TODO TOGETHER: Set the following array equal to `pies` - then console.log `pies`
 // ["apple", "cherry", "key lime", "huckleberry"];
-
+var pies = ["apple", "cherry", "key lime", "huckleberry"];
+// console.log(pies);
 
 // TODO TOGETHER: Set the following array equal to variable called `shapes`. Console.log the variable
 //  ['square', 'rectangle', 'circle', 'triangle'];
-
-
+var shapes = ['square', 'rectangle', 'circle', 'triangle'];
+// console.log(shapes);
+//
 // TODO: Set the following array equal to a variable called `instructors` - then console.log the variable
 //  ["sophie", "vivian", "david", "justin", "daniel", "fernando", "trant"]
-
+var instructors = ["sophie", "vivian", "david", "justin", "daniel", "fernando", "trant"];
+// console.log(instructors);
 
 // TODO: Set the following array equal to a variable called `daysOfTheWeek` - then console.log the variable
 //  ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
-
+var daysOfTheWeek = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+// console.log(daysOfTheWeek);
 
 // TODO: create an array of favorite foods and set to a variable called `favoriteFoods` - then console.log the variable
-
-
+var favoriteFoods = ['cheese', 'milk', 'eggs', 'bacon'];
+// console.log(favoriteFoods)
 
 /* ***************************************************************
 * 					COUNTING AN ARRAY
@@ -54,11 +59,16 @@ console.log("Welcome to intro to arrays");
 //We can also count the length of an array by using the `.length` property
 
 // TODO TOGETHER: Log the length of shapes array
+// console.log("The length of shapes array is: " + shapes.length);
 
 // TODO: console.log the length of the instructors array
-// TODO: console.log the length of the daysOfTheWeek array
-// TODO: console.log the length of the favoriteFoods array
+// console.log("The length of instructors array is: " + instructors.length);
 
+// TODO: console.log the length of the daysOfTheWeek array
+// console.log("The length of daysOfTheWeek array is: " + daysOfTheWeek.length);
+
+// TODO: console.log the length of the favoriteFoods array
+// console.log("The length of favoriteFoods array is: " + favoriteFoods.length);
 
 
 /* ***************************************************************
@@ -74,12 +84,29 @@ console.log("Welcome to intro to arrays");
 // 	console.log(shapes[4]);
 
 // TODO: console.log each element of the instructors array
-// TODO: console.log the first three elements of the daysOfTheWeek array
-// TODO: console.log the first three elements of the favoriteFoods array
+// console.log(instructors[0]);
+// console.log(instructors[1]);
+// console.log(instructors[2]);
+// console.log(instructors[3]);
+// console.log(instructors[4]);
+// console.log(instructors[5]);
+// console.log(instructors[6]);
+// console.log(instructors[7]);
 
+// TODO: console.log the first three elements of the daysOfTheWeek array
+// console.log(daysOfTheWeek[0]);
+// console.log(daysOfTheWeek[1]);
+// console.log(daysOfTheWeek[2]);
+
+// TODO: console.log the first three elements of the favoriteFoods array
+// console.log(favoriteFoods[0]);
+// console.log(favoriteFoods[1]);
+// console.log(favoriteFoods[2]);
 
 // TODO: create a function called `returnLastElement` that accepts an array and returns the last element of that array
-
+// function returnLastElement(array){
+//   return array[array.length - 1];
+// }
 
 
 /* ***************************************************************
@@ -89,19 +116,47 @@ console.log("Welcome to intro to arrays");
 
 
 // TODO TOGETHER: Using a for loop, iterate through the shapes array and log each shape
-
+// for(var i = 0; i < shapes.length; i++){
+//     console.log("***********************");
+//     console.log("current iteration is " + i);
+//     console.log(shapes[i])
+// }
 
 // TODO TOGETHER: Alert "that's my favorite shape!" when your favorite shape is iterated over in the loop.
-
+// for(var i = 0; i < shapes.length; i++){
+//     if(shapes[i] === "triangle"){
+//         console.log("****************");
+//         console.log("Iteration " + i);
+//         // alert(shapes[i] + "! That's my favorite shape!");
+//     }
+// }
 // TODO: What happens if we change var i = 1? or var i = 2;
+// loop starts higher
 // TODO: What are benefits of using loops to iterate?
+//it helps replace repeated code
 // TODO: How does the loop know when to stop iterating?
-
+//when it meets the condition that's set
 // TODO: Using a for loop, iterate through the instructors array and console.log each instructor
+// for(var i = 0; i < instructors.length; i++){
+//     console.log(instructors[i]);
+// }
 // TODO: Using a for loop, iterate through the daysOfTheWeek array and console.log each day of the week
-// TODO: Using a for loop, iterate through the favoriteFoods array and console.log each favorite food
+// for(var i = 0; i < daysOfTheWeek.length; i++){
+//     console.log(daysOfTheWeek[i]);
+// }
 
+// TODO: Using a for loop, iterate through the favoriteFoods array and console.log each favorite food
+// for(var i = 0; i < favoriteFoods.length; i++){
+//     console.log(favoriteFoods[i])
+// }
 // TODO: Refactoring the instructor loop, alert "hey, I know <INSTRUCTOR NAME HERE>" if you have had class with that instructor. If you have not had class with that instructor, alert "I haven't had class with <INSTRUCTOR NAME HERE> yet!"
+// for(var i = 0; i < instructors.length; i++){
+//     if(instructors[i] === "sophie" || instructors[i] === "vivian") {
+//         console.log("Hey, I know " + instructors[i]);
+//     }else{
+//         console.log("I haven't had class with " + instructors[i] + " yet!");
+//     }
+// }
 
 
 
@@ -110,12 +165,28 @@ console.log("Welcome to intro to arrays");
 *************************************************************** */
 
 // TODO TOGETHER: Using a for each loop, console.log each shape from the shapes array
+// shapes.forEach(function (shape,index,array){
+//     console.log(shape);
+//     console.log(index);
+//     console.log(array);
+// })
 
+// TODO TOGETHER: Using a for each loop, console.log each element from the following array: var pies = ["apple", "cherry", "key lime", "huckleberry"];
+// pies.forEach(function(pie){
+//     console.log(pie);
+// })
 
-// TODO TOGETHER: Using a for each loop, console.log each element from the following array:
-var pies = ["apple", "cherry", "key lime", "huckleberry"];
 
 // TODO: Using a for each loop, iterate through the instructors array and console.log each instructor
+instructors.forEach(function (instructor){
+    console.log(instructor);
+})
 // TODO: Using a for each loop, iterate through the daysOfTheWeek array and console.log each day of the week
-// TODO: Using a for each loop, iterate through the favoriteFoods array and console.log each favorite food
+daysOfTheWeek.forEach(function (dayOfTheWeek){
+    console.log(dayOfTheWeek);
+})
 
+// TODO: Using a for each loop, iterate through the favoriteFoods array and console.log each favorite food
+favoriteFoods.forEach(function (favoriteFood){
+    console.log(favoriteFood);
+})
