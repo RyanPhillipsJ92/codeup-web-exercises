4561cf76c1441f9d299c595ceb3dd4548c085c7e
Ryan Phillips <ryan.phillips.j92@gmail.com>
2020-10-09 12:13:45 -0500

    finished circle exercise

---
+++ js/circles.js
@@ -0,0 +1,44 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+
+        getArea: function () {
+           var area = Math.PI * Math.pow(circle.radius,2);
+            return area;
+
+
+        },
+
+        logInfo: function (doRounding) {
+            if(doRounding === true) {
+
+                let rounded = Math.round(circle.getArea());
+                return console.log("Area of a circle with radius rounded: " + this.radius + ", is: " + rounded);
+            }else {
+                return console.log("Area of a circle with radius: " + this.radius + ", is: " + circle.getArea());
+            }
+        }
+    };
+// TODO: complete this method.
+    // TODO: return the proper value
+// If doRounding is true, round the result to the nearest integer.
+// Otherwise, output the complete value
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+circle.radius = 5;
+
+// log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+})();
---
+++ js/math-object-lec.js
@@ -0,0 +1,34 @@
+// Math.random()
+// var mathComputations = new Math(); // Don't do this!!!
+let randomNum = Math.random() * 100;
+// console.log(randomNum);
+
+// Math.round();
+// console.log(Math.round(randomNum)); ///Rounds to the nearest whole number
+
+// Math.floor();
+index = Math.floor(randomNum); //Rounds to the lowest whole number
+// console.log(index);
+
+//Math.ceil();
+// let randomWord = input
+let random1 = (Math.random()* 10 +20).toFixed();
+console.log(random1);
+console.log((Math.random()* 10 +20).toFixed());
+console.log((Math.random()* 10 +20).toFixed());
+console.log((Math.random()* 10 +20).toFixed());
+console.log((Math.random()* 10 +20).toFixed());
+console.log((Math.random()* 10 +20).toFixed());
+console.log((Math.random()* 10 +20).toFixed());
+console.log((Math.random()* 10 +20).toFixed());
+console.log((Math.random()* 10 +20).toFixed());
+let random = (Math.random()* 1000 +2000).toFixed();
+console.log(random);
+console.log((Math.random()* 1000 +2000).toFixed());
+console.log((Math.random()* 1000 +2000).toFixed());
+console.log((Math.random()* 1000 +2000).toFixed());
+console.log((Math.random()* 1000 +2000).toFixed());
+console.log((Math.random()* 1000 +2000).toFixed());
+console.log((Math.random()* 1000 +2000).toFixed());
+console.log((Math.random()* 1000 +2000).toFixed());
+console.log((Math.random()* 1000 +2000).toFixed());
