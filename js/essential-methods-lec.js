c728a466b7042e4c7b7399bffff3716a156f0b01
Ryan Phillips <ryan.phillips.j92@gmail.com>
2020-11-13 13:51:31 -0600

    working on traversing exercise

--- js/essential-methods-lec.js
+++ js/essential-methods-lec.js
@@ -144,9 +144,12 @@ $(document).ready(function() {
      *********************************************/
 
     //TODO TOGETHER: Using the each method, highlight every other list item in the Ravenclaw house
-    //TODO TOGETHER: Change the font color of the first list element in Hufflepuff
-    //TODO TOGETHER: Change the font color of the last list element in Slytherin
 
+    //TODO TOGETHER: Change the font color of the first list element in Hufflepuff
+        $('#hufflepuff').children().first().css('color', 'blue');
+
+    //TODO TOGETHER: Change the font color of the last list element in Slytherin
+    $('#slytherin').children().last().css('color', 'blue');
     //TODO TOGETHER: When the 'Witches' button is clicked, highlight all names that have been assigned the class of 'witches'
     $('#highlight-witches').click(function(e){
         $('li').each(function(){
