6bc2208294c117727b7b446622c399c7ebcd38a7
Ryan Phillips <ryan.phillips.j92@gmail.com>
2020-11-20 16:51:52 -0600

    MFR exercise

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,73 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+const usersWith3Languages = users.filter(user => user.languages.length > 2);
+console.log(usersWith3Languages);
+
+
+let emailArr = users.map(x => x.email);
+console.log(emailArr)
+
+
+const totalYears = users.reduce((years, user) => {
+    return years + user.yearsOfExperience;
+},0);
+let averageExperience = totalYears/users.length;
+console.log(`total years worked by all : ${totalYears}`)
+console.log(`total of people counted : ${users.length}`)
+console.log(`the average experience : ${averageExperience}`);
+
+
+const longestEmail = users.reduce((currentEmail, user) => {
+    if (user.email.length > currentEmail.length){
+        currentEmail = user.email
+    }
+    return currentEmail
+}, "");
+console.log(longestEmail);
+
+const userNames = users.reduce((names, user) => {
+    return `${names} ${user.name}`
+}, 'Your instructors are :');
+
+console.log(userNames);
+
+const singleString = users.reduce(function (currentstring, instructors){
+    return ` ${currentstring}  ${instructors.name}`
+}, 'your instructors are')
+console.log(singleString)
--- js/mfr-lec.js
+++ js/mfr-lec.js
@@ -16,12 +16,23 @@ const numbers = [1,2,3,4,5,6,7,8,9,10];
 
 
 // TODO TOGETHER: Let's map through the array of numbers and add 1 to each element. Console log the new copy of the array.
-
-
+// let newArray = numbers.map(function (num){
+//    return num + 1;
+// });
+// console.log(newArray);
+// console.log(numbers);
 //TODO: Map through the new array of numbers and this time multiply each element by 3. Console log the new copy.
 
-//Bonus: Refactor your functions using ES6
+// let newestArray = newArray.map(function (num){
+//    return num * 3;
+// });
+// console.log(newestArray);
 
+
+
+//Bonus: Refactor your functions using ES6
+// let newestArray = newArray.map(num => num * 3)
+// console.log(newestArray)
 
 /*********************************************
  *              .filter
@@ -29,10 +40,22 @@ const numbers = [1,2,3,4,5,6,7,8,9,10];
 const binary = [1,1,1,1,0,0,1,1,1,1];
 
 // TODO TOGETHER: Let's filter through our binary and return all the true values. We'll store the new array in a variable named 'ones'
-
+// const ones = binary.filter(function (x){
+//     return x
+// })
+// console.log(ones)
 // TODO: filter through binary again and this time, return all the false values.
+// const zeroes = binary.filter(function (x){
+//    return !x;
+// });
+// console.log(zeroes)
 
 //Bonus: Refactor your functions using ES6
+
+// const ones = binary.filter(num => num)
+// console.log(ones);
+// const zeroes = binary.filter(num => !num)
+// console.log(zeroes);
 
 const fruitsAndVeggies = [
     {
@@ -63,18 +86,36 @@ const fruitsAndVeggies = [
 
 
 // TODO: filter through the array of objects and return all fruit type objects.
+const fruits = fruitsAndVeggies.filter(function(produce) {
+    return produce.type === 'fruit';
+});
+console.log(fruits);
 
+const fruitsES6 = fruitsAndVeggies.filter(produce => produce.type === 'fruit');
+console.log(fruitsES6);
 
 // TODO: filter through the array of objects and return all vegetable type objects.
+const vegetables = fruitsAndVeggies.filter(function(produce) {
+    return produce.type === 'vegetable';
+});
+console.log(vegetables);
 
+const vegetablesES6 = fruitsAndVeggies.filter(produce => produce.type === 'vegetable');
+console.log(vegetablesES6);
 
 /*********************************************
  *              .reduce
  ******************************************** */
 
 const numbersArray = [1, 2, 3, 4, 5];
-
-
+// TODO TOGETHER: Let's reduce our original numbers Array into one single value.
+let i = 1; // not needed just to show iterations
+const sum = numbersArray.reduce(function (currentSum,currentNumber){
+    console.log(currentSum + ' current sum on iteration ' + i)
+    console.log(currentNumber + ' current num on iteration ' + i)
+    i = i + 1;
+    return currentSum + currentNumber;
+}, 0)
 //accumulation = 0
 // add 1
 //accumulation = 1
@@ -87,11 +128,18 @@ const numbersArray = [1, 2, 3, 4, 5];
 //add 5
 
 // final accumulation = 15
+console.log(sum);
 
-
-// TODO TOGETHER: Let's reduce our original numbers Array into one single value.
 
 // TODO: Using .reduce, subtract all numbers in the numbers Array from a starting point of 100.
+
+// const diff = numbersArray.reduce(function (currentDiferrence, currentNumber){
+//     return currentDiferrence - currentNumber;
+// },100);
+// console.log(diff)
+//
+// const diffES6 = numbersArray.reduce((currentDiferrence ,currentNumber) => (currentDiferrence - currentNumber),100);
+// console.log(diffES6)
 
 const shoppingCarts = [
     {
@@ -118,15 +166,42 @@ const shoppingCarts = [
 ];
 
 // TODO: Using .reduce, return the total number of apples.
+const totalApples = shoppingCarts.reduce((apples, cart) => {
+    return apples + cart.apples;
+}, 0);
+console.log(totalApples);
 
 
 const colors = ['red','orange','red','blue','blue','green','red'];
 
 // TODO: Count the number of times a color appears in this Array. Hint: your initial value should be an empty object.
 
+const colorCount = colors.reduce(function (colorCount, color){
+    if (typeof colorCount[color] === "undefined"){
+        colorCount[color] = 1;
+    }else {
+        colorCount[color] += 1;
+    }
+
+    return colorCount;
+},{});
+console.log(colorCount);
+
+
+
 
 const lyrics = ['we','all','live','in','a','yellow','submarine'];
 
+
+const oneLine = lyrics.reduce(function (currentString, word){
+   return `${currentString} ${word}`;
+});
+console.log(oneLine);
+
+const oneLineES6 = lyrics.reduce((currentString, word) =>
+    `${currentString} ${word}`);
+
+console.log(oneLineES6);
 //TODO TOGETHER: Using reduce, let's turn this into a string.
 
 // Bonus: Create an Array of all the unique fur colors! Hint: check out the ES6 'Set' data type.
@@ -164,3 +239,6 @@ var hamsters = [
         dateOfBirth: "January 14"
     }
 ];
+
+
+
